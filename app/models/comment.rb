e9cfae9790597d53c496cca4eb7d4b91a4791c0e
Emmanu Varghese <emmanuvarghese1991@gmail.com>
2022-05-14 09:40:49 +0530

    Reactions(emojis) added to comments.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,9 @@ class Comment < ApplicationRecord
   belongs_to :user
 
   has_rich_text :content
+  has_many :emotes, dependent: :destroy
+
+  def emotes_size(key)
+    self.emotes.select { |e| e.emoji == key }.size
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 require "sidekiq/web"
 
 Rails.application.routes.draw do
-  resources :comments
+  resources :comments do
+    resource :emote, only: :show
+  end
   resources :articles
   mount Sidekiq::Web => "/sidekiq"
 
