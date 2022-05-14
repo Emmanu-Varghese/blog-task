55eb17cea7d0e054cdfa37c8b71aa3c1e545e345
Emmanu Varghese <emmanuvarghese1991@gmail.com>
2022-05-14 09:19:04 +0530

    comments styling updated

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -23,7 +23,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to comment_url(@comment), notice: "Comment was successfully created." }
+        format.html { redirect_to comments_url, notice: "Comment was successfully created." }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -64,6 +64,6 @@ class CommentsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def comment_params
-    params.require(:comment).permit(:commentable_id, :commentable_type, :user_id)
+    params.require(:comment).permit(:commentable_id, :commentable_type, :user_id, :content)
   end
 end
