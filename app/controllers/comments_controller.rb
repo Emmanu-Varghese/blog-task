a3fdfa098e14249fa996297e2fd0cef924da760d
Emmanu Varghese <emmanuvarghese1991@gmail.com>
2022-05-14 21:49:07 +0530

    article feature test completed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -50,6 +50,12 @@ class CommentsController < ApplicationController
   # DELETE /comments/1 or /comments/1.json
   def destroy
     @comment.destroy
+
+    respond_to do |format|
+      format.html { redirect_to comments_url, notice: "Comment was successfully destroyed." }
+      format.json { head :no_content }
+      format.js { render layout: false }
+    end
   end
 
   private
